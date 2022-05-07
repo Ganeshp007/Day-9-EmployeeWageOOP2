e50b3df73ded9a1f43cc8e8728d55f918a9724b9
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-07 19:49:03 +0530

    Added: UC3_AddParttimeEmpWage

--- Day-9-EmployeeWageOOP2/CompanyEmpWageBuilder.cs
+++ Day-9-EmployeeWageOOP2/CompanyEmpWageBuilder.cs
@@ -9,7 +9,8 @@ namespace Day_9_EmployeeWageOOP2
     internal class CompanyEmpWageBuilder
     {
 
-        public const int IS_FUlltime = 1;
+        public const int IS_Parttime = 1;
+        public const int IS_Fulltime = 2;
         public const int EmpRatePerHr = 20;
         public void EmpCheck(string Company)
         {
@@ -17,28 +18,32 @@ namespace Day_9_EmployeeWageOOP2
 
 
             Random r = new Random();
-            int empcheck = r.Next(0, 2);
+            int empcheck = r.Next(0, 3);
 
             switch (empcheck)
             {
-                case IS_FUlltime:
+                case IS_Fulltime:
                     emphr = 8;
-                    Console.WriteLine(">> Employee of " + Company + " is Present....\n");
+                    Console.WriteLine(">> Employee of " + Company + " is Present and it is Fulltime Employee ....\n");
 
                     break;
+                case IS_Parttime:
+                    emphr = 4;
+                    Console.WriteLine(">> Employee of " + Company + " is Present and it is Parttime Employee ....\n");
+                    break;
 
-                 
                 default:
                     emphr = 0;
                     Console.WriteLine(">> Employee of " + Company + " is Absent....\n");
                     break;
             }
-
             int Dailywage = EmpRatePerHr * emphr;
 
 
             Console.WriteLine("Employee Wage of a day to " + Company + " company for current Employee is :- " + Dailywage + "\n");
         }
+
+
 
 
     }
